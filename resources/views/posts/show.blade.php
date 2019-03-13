24afc76dcf7648276265f53c51ce7b0bf7637afe
mahydit <mahydti_anber@outlook.com>
2019-03-13 21:14:14 +0200

    adding post creator section

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,12 +2,22 @@
 
 @section('content')
 
-<!-- TODO: show post creator with specific date formate -->
-<div class="jumbotron">
-  <h1 class="display-4">{{$post->title}}</h1>
-  <hr class="my-4">
-  <p>{{$post->description}}</p>
+  <div class="card">
+    <h5 class="card-header">Post Information</h5>
+    <div class="card-body">
+      <h5 class="card-title">{{$post->title}}</h5>
+      <p class="card-text">{{$post->description}}</p>
+    </div>
+  </div>
+
+  <div class="card" style="margin-top:20px;margin-bottom:20px;">
+    <h5 class="card-header">Creator Information</h5>
+    <div class="card-body">
+      <h5 class="card-title">Name: {{$user->name}}</h5>
+      <h5 class="card-title">Email: {{$user->email}}</h5>
+      <h5 class="card-title">Created at: {{$user->created_at}}</h5>
+    </div>
+  </div>
   <a href="{{route('posts.index')}}" class="btn btn-danger">Back</a>
-</div>
 
 @endsection
