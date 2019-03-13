4279aae3dc42e25b41d6cc06e15b9adddc216b0d
mahydit <mahydti_anber@outlook.com>
2019-03-13 21:30:17 +0200

    bug fix

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -13,6 +13,7 @@
            <label for="exampleInputPassword1">Description</label>
            <textarea name="description" class="form-control">{{$post->description}}</textarea>
        </div>
+       
        <div class="form-group">
            <label for="exampleInputPassword1">Post Creator</label>
            <select class="form-control" name="user_id">
@@ -20,7 +21,7 @@
                 @if($user->id == $post->user_id)
                    <option value="{{$user->id}}" selected>{{$user->name}}</option>
                 @else
-                   <option value="{{$user->id}}" selected>{{$user->name}}</option>
+                   <option value="{{$user->id}}" >{{$user->name}}</option>
                 @endif
                @endforeach
            </select>
