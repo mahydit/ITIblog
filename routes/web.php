d0e7eae5d7dab6e63b6b79b7be849e596d545996
mahydit <mahydti_anber@outlook.com>
2019-03-14 14:32:35 +0200

    updating web.routes with middleware auth

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,32 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/posts', 'PostsController@index')->name('posts.index');
-Route::get('/posts/create', 'PostsController@create')->name('posts.create');
-Route::post('/posts','PostsController@store')->name('posts.store');
-Route::get('/posts/{post}/edit','PostsController@edit')->name('posts.edit');
-Route::get('/posts/{post}','PostsController@show')->name('posts.show');
-Route::delete('/posts/{post}','PostsController@destroy')->name('posts.destroy');
-Route::put('/posts/{post}','PostsController@update')->name('posts.update');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/posts', 'PostsController@index')
+    ->name('posts.index');
+
+    Route::get('/posts/create', 'PostsController@create')
+    ->name('posts.create');
+
+    Route::post('/posts','PostsController@store')
+    ->name('posts.store');
+
+    Route::get('/posts/{post}/edit','PostsController@edit')
+    ->name('posts.edit');
+
+    Route::get('/posts/{post}','PostsController@show')
+    ->name('posts.show');
+
+    Route::delete('/posts/{post}','PostsController@destroy')
+    ->name('posts.destroy');
+
+    Route::put('/posts/{post}','PostsController@update')
+    ->name('posts.update');
+});
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
