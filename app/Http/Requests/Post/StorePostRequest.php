20e2e30ca27fac2a034c88320eceb8415db1f801
mahydit <mahydti_anber@outlook.com>
2019-03-15 09:30:44 +0200

    updating StorePostRequest and UpdatePost Request with user_id validation

--- app/Http/Requests/Post/StorePostRequest.php
+++ app/Http/Requests/Post/StorePostRequest.php
@@ -25,7 +25,9 @@ class StorePostRequest extends FormRequest
     {
         return [
            'title' => 'required|min:3|unique:posts,title',
-           'description' => 'required|min:10'
+           'description' => 'required|min:10',
+           'user_id'=> 'exists:posts,user_id'
+
        ];
     }
 
--- app/Http/Requests/Post/UpdatePostRequest.php
+++ app/Http/Requests/Post/UpdatePostRequest.php
@@ -25,7 +25,8 @@ class UpdatePostRequest extends FormRequest
     {
         return [
             'title' => 'required|min:3|unique:posts,title,'.$this->post,
-            'description' => 'required|min:10'
+            'description' => 'required|min:10',
+            'user_id'=> 'exists:posts,user_id'
         ];
     }
 }
